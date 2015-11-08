58dcf04c5db47b0ea145e867aa5ba89e4d5168ca
Fraser, James <James.Fraser@dsto.defence.gov.au>
2015-11-08 14:26:12 +1100

    added some file work from last lecture

---
+++ files.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(void) {
+    ifstream i;
+    ofstream o;
+
+    string fileName = "test.txt";
+    string test;
+
+    i.open(fileName);
+    getline(i, test);
+    i.close();
+    cout << "FILE READS: " << test << endl;
+
+    string append = " and some more!";
+    test = test.append(append);
+    cout << "appending..." << endl;
+
+    o.open(fileName);
+    o << test;
+    o.close();        
+
+    i.open(fileName);
+    getline(i, test);
+    i.close();
+    cout << "FILE NOW READS: " + test << endl;
+}
+
