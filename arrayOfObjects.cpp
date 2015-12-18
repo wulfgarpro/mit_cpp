eeedea7cde96f0107c7c9e316a94e9c58a0134eb
James Fraser <wulfgar.pro@gmail.com>
2015-12-18 12:19:26 +1100

    played with constructor with obj array

--- arrayOfObjects.cpp
+++ arrayOfObjects.cpp
@@ -8,18 +8,24 @@ class Person {
         string name;
 
         Person() {
+            cout << "constructor called!" << endl;
             name = "James";
         }
 };
 
 int main(void) {
-    Person* parray = new Person[4];
+    Person* parray = new Person[4]; // implicit call of Person constructor
     int size = (sizeof(*parray)/sizeof(parray));
-    
+
     for(int count=0; count < size; count++) {
         cout << parray[count].name << endl;
     }
 
+    Person* parrayB[5]; // doesn't call constructor
+    parrayB[0] = new Person; // must do it manually
+    Person me;
+    parrayB[1] = &me;
+
+    cout << parrayB[0]->name << endl;
     return 0;
 }
-
