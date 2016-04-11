b82f3c024c88bd67a1d65ad7e6b05f0fc1843d99
James Fraser <wulfgar.pro@gmail.com>
2016-04-11 21:35:09 +1000

    started fleshing out rectangle child class

--- assignment_3/polygon/rectangle.h
+++ assignment_3/polygon/rectangle.h
@@ -1,5 +1,13 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
 #include "polygon.h"
 
 class Rectangle : Polygon {
+    Rectangle(const Point lowerLeft, const Point upperRight);
+    Rectangle(int lowerLeftX, int lowerLeftY, int upperRightX, int upperRightY);
+    double area();
 };
+
+#endif
 
