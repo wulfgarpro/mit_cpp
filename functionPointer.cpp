3f126c0018690a92515082730178b5bd220d13a5
James Fraser <wulfgar.pro@gmail.com>
2016-05-13 11:30:56 +1000

    updated fp example with c++ syntax

--- functionPointer.cpp
+++ functionPointer.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 #include <string.h>
 
+// C++ STYLE
+class MyClass {
+    private:
+    protected:
+    public:
+        void hello(std::string name) {
+            std::cout << "Hello " << name << std::endl;
+        }
+        void sayHello(std::string name, void (MyClass::*helloPtr)(std::string)) {
+            (this->*helloPtr)(name);
+        }
+};
+
+// C STYLE
 void hello(std::string name) {
     std::cout << "Hello " << name << std::endl;
 }
-
 void sayHello(std::string name, void (*helloPtr)(std::string)) {
     helloPtr(name);
 }
 
 int main(void) {
+    // c style
     sayHello("James", &hello);
+
+    // c++ with this
+    MyClass mc;
+    mc.sayHello("James", &MyClass::hello);
 
     return 0;
 }
