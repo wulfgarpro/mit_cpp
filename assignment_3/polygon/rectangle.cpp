59794a447501f6635f3108cce1a846ffb3414aff
James Fraser <wulfgar.pro@gmail.com>
2016-06-14 10:40:04 +1000

    added ignores

--- assignment_3/polygon/rectangle.cpp
+++ assignment_3/polygon/rectangle.cpp
@@ -14,30 +14,3 @@ double Rectangle::area() const {
     return 5.5; // TODO 
 };
 
-/* 
-int main(void) {
-    std::cout << "Polygon go..." << std::endl;
-    std::cout << Polygon::getNumPolygons() << std::endl;
-
-    Rectangle* r1 = new Rectangle(0,0,5,5);
-
-    std::cout << Polygon::getNumPolygons() << std::endl;
-
-    std::cout << "Rec area: " << r1->area() << std::endl;
-    std::cout << "Rec sides count: " << r1->getNumSides() << std::endl;
-
-    Rectangle* r2 = new Rectangle(5,5,0,0);
-
-    std::cout << "Rec area: " << r2->area() << std::endl;
-    std::cout << "Rec sides count: " << r2->getNumSides() << std::endl;
-
-    std::cout << Polygon::getNumPolygons() << std::endl;
-
-    delete r1;
-    std::cout << Polygon::getNumPolygons() << std::endl;
-    delete r1;
-    std::cout << Polygon::getNumPolygons() << std::endl;
-    
-    return 0;
-};
-*/
