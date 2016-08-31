a5589d9de7de18055980f6ff17dc8e9edc2c8b90
James Fraser <wulfgar.pro@gmail.com>
2016-08-31 19:50:15 +1000

    passed WordWithConsonantIsPiggedProperly

--- assignment_3/strings/word.cpp
+++ assignment_3/strings/word.cpp
@@ -1,6 +1,7 @@
 #include "word.h"
 #include <string>
 #include <algorithm>
+#include <iostream>
 
 Word::Word(const std::string word) {
     this->word = word;
@@ -11,13 +12,24 @@ std::string Word::pigLatinify() {
     bool consonant = isConsonant();
     // 2. call the appropriate helper
     if(consonant) return pigLatinifyConsonant();
-    return pigLatinifyVowel();
+    return pigLatinifyConsonant();
 };
 
 std::string Word::pigLatinifyConsonant() {
+    std::string tmp = "";
+
     // 1. move init consonant to end of word (qu is 1 consonsnt)
-    // 2. add an "ay" to the end of the word
-    return NULL;
+    char c = this->word[0];
+    if(c == 'q' && this->word[1] == 'u') {
+        tmp = this->word.substr(2, this->word.length() - 2) + '-';
+        tmp += "qu";
+    } else {
+        tmp = this->word.substr(1, this->word.length() - 1) + '-';
+        tmp += c;
+    }
+    // 2. add an "-ay" to the end of the word
+    tmp += "ay";
+    return tmp;
 };
 
 std::string Word::pigLatinifyVowel() {
