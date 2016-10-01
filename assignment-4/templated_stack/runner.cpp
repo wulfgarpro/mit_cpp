03ffcaa8e3afd058ae339399b61f6cfd8f0d04f5
James Fraser <wulfgar.pro@gmail.com>
2016-10-01 21:39:51 +1000

    started 4_2 non-member friend operator overload - not working atm

--- assignment-4/templated_stack/runner.cpp
+++ assignment-4/templated_stack/runner.cpp
@@ -1,28 +1,26 @@
 #include "templated_stack_4_1.h"
 #include <iostream>
 
+template<typename T>
+void addToStack(Stack<T>& s) {
+}
+
 int main(void) {
     Stack<int> myStack;
-
-    const int& one = 1;
-    const int& two = 2;
-    const int& three = 3;
-    const int& four = 4;
-    const int& five = 5;
-
     std::cout << "Stack is empty: " << myStack.empty() << std::endl;
+    // Add to stack
+    s.push(1);
+    std::cout << s.top() << std::endl;
+    s.push(2);
+    std::cout << s.top() << std::endl;
+    s.push(3);
+    std::cout << s.top() << std::endl;
+    s.push(4);
+    std::cout << s.top() << std::endl;
+    s.push(5);
+    std::cout << s.top() << std::endl;
 
-    myStack.push(one);
-    std::cout << myStack.top() << std::endl;
-    myStack.push(two);
-    std::cout << myStack.top() << std::endl;
-    myStack.push(three);
-    std::cout << myStack.top() << std::endl;
-    myStack.push(four);
-    std::cout << myStack.top() << std::endl;
-    myStack.push(five);
-    std::cout << myStack.top() << std::endl;
-
+    // Pop from stack
     myStack.pop();
     std::cout << myStack.top() << std::endl;
     myStack.pop();
@@ -30,21 +28,41 @@ int main(void) {
     myStack.pop();
     std::cout << myStack.top() << std::endl;
     myStack.pop();
-    std::cout << myStack.top() << std::endl;
-
-    myStack.pop();
-    myStack.pop();
-    myStack.pop();
-    myStack.pop();
-    myStack.pop();
-    
-    //std::cout << myStack.top() << std::endl; // error
-
-    myStack.push(five);
     std::cout << myStack.top() << std::endl;
 
+    // ... and some more
+    myStack.pop();
+    myStack.pop();
+    myStack.pop();
+    myStack.pop();
+    myStack.pop();
+
+    //std::cout << myStack.top() << std::endl; // error be design when empty
+
+    // Just add 1 item back to stack
+    myStack.push(1);
+    std::cout << myStack.top() << std::endl;
+
+    // Now take it away
     myStack.pop();
     std::cout << "Stack is empty: " << myStack.empty() << std::endl;
 
+    // Add to stack
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+
+    // Create a second stack for + overload
+    Stack<int> myStackB;
+    myStackB.push(1);
+    myStackB.push(2);
+    myStackB.push(3);
+    myStackB.push(4);
+    myStackB.push(5);
+
+    // testing + operator overload
+    Stack<int> super = myStack + myStackB;        
     return 0;
 }
--- assignment-4/templated_stack/templated_stack_4_1.h
+++ assignment-4/templated_stack/templated_stack_4_1.h
@@ -1,24 +1,30 @@
+#ifndef TEMPLATED_STACK_H
+#define TEMPLATED_STACK_H
+
 #include <vector>
 #include <iostream>
 
 template<typename T>
+class Stack;
+
+template<typename T>
+Stack<T> operator+ (const Stack<T>& a, const Stack<T>& b);
+
+template<typename T>
 class Stack {
     public:
-        Stack() { std::cout << "constructing stack" << std::endl; stack = new std::vector<T>(); };
+        Stack() : stack(new std::vector<T>()) {};
         ~Stack() { delete stack; };
 
         bool empty() {
             if(stack->empty()) return true;
             return false; 
         };
-
         void push(const T &item) {
             stack->insert(stack->begin(), item);
         };
 
-        T& top() {
-            return stack->front();
-        };
+        T& top() { return stack->front(); };
 
         void pop() {
             if(!empty()) {
@@ -27,8 +33,25 @@ class Stack {
             }
         };
 
+        friend Stack operator+ <>(const Stack&, const Stack&);
     protected:
-
     private:
         std::vector<T>* stack;
 };
+
+template<typename T>
+Stack<T> operator+(const Stack<T>& a, const Stack<T>& b) {
+    Stack<T> s;
+    std::vector<T> tmp = *(b.stack);
+    decltype typedef typename std::vector<T>::iterator it = tmp.begin();
+
+    /*for(it = b.stack->begin(); it != b.stack->end(); it++) {
+
+    }*/
+
+    std::cout << a.stack->size() << std::endl;
+    std::cout << b.stack->size() << std::endl;
+    return s; 
+};
+
+#endif // TEMPLATED_STACK_H
