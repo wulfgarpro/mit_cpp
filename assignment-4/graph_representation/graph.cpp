034dc8ba5c3788e50c527047b497a7482970a1e7
James Fraser <wulfgar.pro@gmail.com>
2016-10-30 23:25:45 +1100

    some optimisations

--- assignment-4/graph_representation/graph.cpp
+++ assignment-4/graph_representation/graph.cpp
@@ -5,7 +5,6 @@
 
 Graph::Graph(const std::vector<int>& starts, const std::vector<int>& ends) {
     if (starts.size() != ends.size()) throw new std::invalid_argument("Start and end vectors must be the same length.");
-
     for (auto i = 0; i < starts.size(); i++) {
         this->graph->insert(std::pair<int, std::vector<int>>(starts.at(i), std::vector<int>()));
         auto *key_vector = &(this->graph->at(starts.at(i)));
@@ -14,17 +13,13 @@ Graph::Graph(const std::vector<int>& starts, const std::vector<int>& ends) {
 }
 
 int Graph::numOutgoing(const int nodeID) const {
-    int count = 0;
-    for (auto val = this->graph->cbegin(); val != this->graph->cend(); val++) {
-        if (val->first == nodeID) count = (*val).second.size();
-    }
-    return count;
+    auto item = this->graph->find(nodeID);
+    if (item == this->graph->end()) throw new std::invalid_argument("Specified node does not exist."); 
+    return (*item).second.size();
 }
 
 const std::vector<int>& Graph::adjacent(const int nodeID) const {
-    for (auto val = this->graph->cbegin(); val != this->graph->cend(); val++) {
-        if (val->first == nodeID) {
-            return (*val).second; 
-        }
-    }
+    auto item = this->graph->find(nodeID);
+    if (item == this->graph->end()) throw new std::invalid_argument("Specified node does not exist."); 
+    return (*item).second;
 }
