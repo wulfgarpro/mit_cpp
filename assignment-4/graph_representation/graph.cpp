4236fe439721a1efe4677aa1332be14e80dead3a
James Fraser <wulfgar.pro@gmail.com>
2016-10-30 18:17:18 +1100

    finished basic impl with test runner

--- assignment-4/graph_representation/graph.cpp
+++ assignment-4/graph_representation/graph.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
 #include <vector>
 #include "graph.h"
+#include <stdexcept>
 
-Graph::Graph(const std::vector<int> &starts, const std::vector<int> &ends) {
-    if (starts.size() == ends.size()) {
-        for (auto i = 0; i < starts.size(); i++) {
-            this->graph->insert(std::pair<int, std::vector<int>>(starts.at(i), std::vector<int>()));
-            auto key_vector = this->graph->at(starts.at(i));
-            key_vector.push_back(ends.at(i));
-        }
+Graph::Graph(const std::vector<int>& starts, const std::vector<int>& ends) {
+    if (starts.size() != ends.size()) throw new std::invalid_argument("Start and end vectors must be the same length.");
+
+    for (auto i = 0; i < starts.size(); i++) {
+        this->graph->insert(std::pair<int, std::vector<int>>(starts.at(i), std::vector<int>()));
+        auto *key_vector = &(this->graph->at(starts.at(i)));
+        key_vector->push_back(ends.at(i));
     }
-    std::cout << this->graph->size() << std::endl;
 }
 
 int Graph::numOutgoing(const int nodeID) const {
-    return -1;
+    int count = 0;
+    for (auto val = this->graph->cbegin(); val != this->graph->cend(); val++) {
+        if (val->first == nodeID) count = (*val).second.size();
+    }
+    return count;
 }
 
 const std::vector<int>& Graph::adjacent(const int nodeID) const {
+    for (auto val = this->graph->cbegin(); val != this->graph->cend(); val++) {
+        if (val->first == nodeID) {
+            return (*val).second; 
+        }
+    }
 }
