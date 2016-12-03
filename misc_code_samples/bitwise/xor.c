429d2a75d8b7842d5182cf2ebe266f514aed0835
James Fraser <wulfgar.pro@gmail.com>
2016-12-03 15:57:49 +1100

    basic xor impl

--- misc_code_samples/bitwise/xor.c
+++ misc_code_samples/bitwise/xor.c
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <cassert>
 
 // demonstrates exclusive or, XOR, (^) with bit field
 int main(void) {
+    // XOR is 1 1 = 0, 1 0 = 1, 0 1 = 1, 0 0 = 0, only 1 of either can be 1, unlike OR, where 1 1 = 1
     // XOR is a way of flipping a specific bit on or off without using the complement operator + & in two seperate stages (see not.c)
+    // Using complement + & is a hacky way of turning a bit on; why would you use a bitmask with the opposites of the items in the original byte to turn a bit on or off if you didn't have to? XOR is a better way
+    // e.g.
+    //
+
+    char item_3 = 1 << 2;
+    assert(item_3 != 0);
+    item_3 ^= item_3; // this flips item_3 only
+    assert(item_3 == 0);
     return 0;
 }
