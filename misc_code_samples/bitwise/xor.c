e32542fc176d52ff320662db414e7e95334cedb9
James Fraser <wulfgar.pro@gmail.com>
2016-12-03 15:44:23 +1100

    xor init

---
+++ misc_code_samples/bitwise/xor.c
@@ -0,0 +1,7 @@
+#include <iostream>
+
+// demonstrates exclusive or, XOR, (^) with bit field
+int main(void) {
+    // XOR is a way of flipping a specific bit on or off without using the complement operator + & in two seperate stages (see not.c)
+    return 0;
+}
