1dc9eff188467e229f059fba0267baa006ab3540
James Fraser <wulfgar.pro@gmail.com>
2016-12-03 16:29:12 +1100

    finished xor challenge

--- misc_code_samples/bitwise/xor_challenge.c
+++ misc_code_samples/bitwise/xor_challenge.c
@@ -1,5 +1,31 @@
-#include <iosteam>
+#include <iostream>
+#include <cassert>
 
 int main(void) {
+    /*int a = 5;
+    int b = 2;
+
+    assert(a == 5);
+    assert(b == 2);
+
+    int tmp = a;
+    a = b;
+    b = tmp;
+
+    assert(a == 2);
+    assert(b == 5);*/
+
+    int a = 5; // 00000101
+    int b = 2; // 00000010
+
+    // how can we XOR these two so that we swap their values without a tmp?
+
+    // A XOR B = C (c is A + B)
+    // A XOR C = B
+    // B XOR C = A
+    a ^= b ^= a ^= b; // a is now b
+    assert(a == 2);
+    assert(b == 5);
+
     return 0;
 }
